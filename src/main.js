c5a881f70479a00696a2c5c0656e3cf9d049ee0e
Umar r <fancycyanogen@gmail.com>
2025-07-24 11:58:47 +0700

    navbar menu mobile

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.navbar__menu');
+  const mobileMenu = document.querySelector('.mobile__menu');
+  const closeMenu = document.querySelector('.mobile__menu__back');
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.add('open');
+    document.body.style.overflow = 'hidden';
+  });
+  closeMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('open');
+    document.body.style.overflow = '';
+  });
+
   const scrollContainer = document.querySelector('.about__inner__body__gallery');
   const scrollLeftBtn = document.querySelector('.scroll-left');
   const scrollRightBtn = document.querySelector('.scroll-right');
