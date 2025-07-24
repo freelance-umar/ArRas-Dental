03584a3e7b9dd4e3233bbde2204e91691dc7fa3c
Umar r <fancycyanogen@gmail.com>
2025-07-24 21:33:20 +0700

    gallery

--- src/main.js
+++ src/main.js
@@ -30,4 +30,41 @@ document.addEventListener('DOMContentLoaded', () => {
       behavior: 'smooth'
     });
   });
+
+  // Gallery Klinik
+    const imageGalleryKlinik = [
+      "image-1.jpg",
+      "image-2.jpg",
+      "image-3.jpg",
+      "image-4.jpg",
+      "image-5.jpg",
+      "image-6.jpg",
+      "image-7.jpg",
+      "image-8.jpg",
+      "image-9.jpg",
+      "image-10.jpg",
+      "image-11.jpg",
+      "image-12.jpg",
+    ]
+    function createKlinikImage(src, alt = 'Klinik Image') {
+      // Create the container div
+      const container = document.createElement('div');
+      container.classList.add('gallery__klinik__img');
+
+      // Create the img element
+      const img = document.createElement('img');
+      img.src = src;
+      img.alt = alt;
+
+      // Append the image into the container
+      container.appendChild(img);
+
+      // Return the full element (so you can append it somewhere)
+      return container;
+    }
+    const gallery = document.querySelector('.gallery__klinik__swiper');
+    imageGalleryKlinik.forEach(filename => {
+      const el = createKlinikImage(`/src/assets/images/optimized/gallery/${filename}`);
+      gallery.appendChild(el);
+    });
 });
