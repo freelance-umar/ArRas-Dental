dff325b5f20d06581e9ebc46c1d7d9e8681f6cb4
Umar r <fancycyanogen@gmail.com>
2025-07-27 13:39:19 +0700

    remove scroll btn that cause swiper js error

--- src/main.js
+++ src/main.js
@@ -11,26 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.style.overflow = '';
   });
 
-  const scrollContainer = document.querySelector('.about__inner__body__gallery');
-  const scrollLeftBtn = document.querySelector('.scroll-left');
-  const scrollRightBtn = document.querySelector('.scroll-right');
-
-  const scrollAmount = 400;
-
-  scrollLeftBtn.addEventListener('click', () => {
-    scrollContainer.scrollBy({
-      left: -scrollAmount,
-      behavior: 'smooth'
-    });
-  });
-
-  scrollRightBtn.addEventListener('click', () => {
-    scrollContainer.scrollBy({
-      left: scrollAmount,
-      behavior: 'smooth'
-    });
-  });
-
     // Swiper
 
     const thumbsSwiper = new Swiper('.floating-thumbs', {
